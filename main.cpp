6f23acda0ffb26ea21c13c6cb4ccd384011d8eee
Tom-Sheiles <36983461+Tom-Sheiles@users.noreply.github.com>
2021-04-15 11:00:35 +1000

    Removed second cube

--- main.cpp
+++ main.cpp
@@ -19,21 +19,15 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     float screenWidth = 1280;
     float screenHeight = 720;
 
-    Engine *engine = new Engine(L"APP", 320, 150, screenWidth, screenHeight, hInstance, CREATE_CONSOLE);
-    Renderer::Init(screenWidth, screenHeight, engine, SHADED | CULLING);
+    Engine *engine = new Engine(L"APP", 0, 20, screenWidth, screenHeight, hInstance, CREATE_CONSOLE);
+    Renderer::Init(screenWidth, screenHeight, engine, WIREFRAME | CULLING);
 
     Material *red = new Material();
     red->SetColor("u_Color", 200, 50, 50, 255);
-    Material *blue = new Material();
-    blue->SetColor("u_Color", 50, 200, 50, 255);
 
     Quad *quad = new Quad(red);
-    quad->position = {3, 0, -5.0f};
+    quad->position = {0, 0, -5.0f};
     quad->scale = {0.5f, 0.5f, 0.5f};
-
-    Quad *quad2 = new Quad(blue);
-    quad2->position = {-3, 0, -5.0f};
-    quad2->scale = {0.5f, 0.5f, 0.5f};
 
     float speed = 5;
 
